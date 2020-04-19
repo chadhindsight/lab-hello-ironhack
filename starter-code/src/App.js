789602adfdf5b6b0b2bc08cd0d5542fd619ac3b2
Chad Hinds <chadrickj8@gmail.com>
2020-04-19 12:47:03 -0400

    First iteration

---
+++ starter-code/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Say hello to ReactJS</h1>
+            <footer>
+
+            </footer> 
+        )
+    }
+}
+
+
+
+
+
+export default App
